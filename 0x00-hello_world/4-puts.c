0a2f699389f32ec06d27487b8dca5bb642059d74
Kurller <kolaquadry@gmail.com>
2022-06-30 17:48:59 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
+#!/bin/bash
+
+
+
 #include <stdio.h>
 int main(){
 
 
-puts("\"Programming is like building a multilingua puzzle");
+puts("Programming is like building a multilingua puzzle\n");
 return 0;
 }
 
