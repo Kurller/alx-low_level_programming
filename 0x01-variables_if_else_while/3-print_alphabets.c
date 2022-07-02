e044eaa4a3633da0bd3bd69450de9f1d8e294618
Kurller <kolaquadry@gmail.com>
2022-07-02 02:27:44 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+int main(void)
+{ char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'q' && ch != 'e')
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
+
+
