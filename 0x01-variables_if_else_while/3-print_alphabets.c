09a76613ff0bbd5edfc1d3d98a02e8f6052255f5
Kurller <kolaquadry@gmail.com>
2022-07-04 01:34:09 -0700

    hot fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,11 @@
 int main(void)
 { char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
-{
-if (ch != 'q' && ch != 'e')
-{
 putchar(ch);
-}
-}
-putchar(10);
+
+for (ch = 'A'; ch <= 'z'; ch++)
+putchar(ch);
+putchar("\n");
 return (0);
 }
 
