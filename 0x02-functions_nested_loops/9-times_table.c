fc7e96132f970dbf82e7618d400fcf5cf8ac84f1
Kurller <kolaquadry@gmail.com>
2022-07-06 00:59:42 -0700

    hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,12 +7,21 @@ void times_table(void)
 {
 int i = 0;
 int j = 0;
+int n;
 for (i = 0; i < 10 ; i++)
 {
-for (j = 0; j < 10; j++)
+for (j = 0; j <= n; j++)
+{
+if (j <= n - 1)
 {
 _putchar(i * j);
 _putchar(' ');
+else
+{
+_putchar(i * j);
+_putchar(i * j);
+}
+}
 }
 }
 }
