ca3bade264d92448a4f02eca70d86740604e51cf
Kurller <kolaquadry@gmail.com>
2022-07-05 18:20:27 -0700

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,7 +6,7 @@ void jack_bauer(void)
 {
 int i, j;
 for (i = 0; i < 24; i++)
-{
+i{
 for (j = 0; j < 60; j++)
 {
 if (i < 10)
@@ -24,13 +24,8 @@ if (j < 10)
 {
 _putchar(':');
 _putchar('0');
-_putchar(j);
-}
-else if (j >= 10)
-{
-_putchar(':');
-_putchar('0');
 _putchar(j + '0');
+
 }
 else if (j > 10)
 {
@@ -39,6 +34,7 @@ _putchar((j / 10) + '0');
 _putchar((j % 10) + '0');
 }
 _putchar('\n');
+}
 }
 }
 }
