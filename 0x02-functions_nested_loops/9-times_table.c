45b6a0846af64b87d288eb342a1c5d5d37810716
Kurller <kolaquadry@gmail.com>
2022-07-05 19:25:32 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,3 +1,18 @@
 
 #include "main.h"
-
+/**
+ * main - print 9 times table stating with 0
+ *
+ * Return:always return 0
+ */
+int main(void)
+{
+int i = 0;
+do {
+printf("9 * %d = %d\n", i, 9 * i);
+i++;
+} while (i <= 10);
+{
+return (0);
+}
+}
