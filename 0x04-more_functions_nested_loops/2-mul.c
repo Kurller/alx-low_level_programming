e7716da6dc286f53464e09df29bbcbd798805344
Kurller <kolaquadry@gmail.com>
2022-07-08 16:29:03 -0700

    multiplication done

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,17 +1,12 @@
 #include "main.h"
-/** 
- * print_numbers - print numbers
- * Return:Always 0
- */
-void print_numbers(void)
+/**
+* mul - function that multiply numbers
+*
+* @a: first number
+* @b: second number
+* Return:returns result
+*/
+int mul(int a, int b)
 {
-int i;
-i = 0;
-while (i < 10)
-{
-_putchar(i + '0');
-i++;
-}
-
-_putchar('\n');
+return (a * b);
 }
