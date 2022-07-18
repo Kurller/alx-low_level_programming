e71efc8c10437dac891f7d320a00b7515a229ef3
Kurller <kolaquadry@gmail.com>
2022-07-18 12:21:02 -0700

    task 2 done

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,4 @@
-#include "main.h"
+ #include "main.h"
 /**
  * _strchr - string character
  * @s: string given
@@ -9,14 +9,18 @@ char *_strchr(char *s, char c)
 {
 int a = 0, b;
 while (s[a])
+{
 	a++;
+}
 for (b = 0; b < a; b++)
 {
 if (c == s[b])
+{
 s += b;
 return (s);
 }
-return ('\0');
+}
+}
 }
 }
 {
