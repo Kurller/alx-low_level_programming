c450e01898d5e4d9d24b9abbffcd7d789be78343
Kurller <kolaquadry@gmail.com>
2022-07-25 00:13:44 -0700

    task 0 done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ int i;
 char *s;
 if (size == 0)
 return (NULL);
-s = char *malloc(size * sizeof(char));
+s = (char *)malloc(size * sizeof(char));
 if (s == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
