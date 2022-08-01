9847aee47ad31b4941b09cedf196a50363f8290d
Kurller <kolaquadry@gmail.com>
2022-08-01 09:08:05 -0700

    task 0 done

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <math.h>
+#include <iostream>
+using namespace std;
+int main(void)
+{
+int val;
+val = -10;
+cout << "Absolute value of " << val << "is " << abs(val) << endl;
+val = 10;
+cout << "Absolute value of " << val << "is " << abs(val) << endl;
+return (0);
+}
