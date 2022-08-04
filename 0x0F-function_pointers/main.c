aa50af304a7350a137857a8b7f0da1675530066a
Kurller <kolaquadry@gmail.com>
2022-08-04 14:01:05 -0700

    task 0 done

--- 0x0F-function_pointers/main.c
+++ 0x0F-function_pointers/main.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 #include "function_pointers.h"
-#include <string.h>
 /**
  * print_name_as_is - prints a name as is
- *
  * @name: name of the person
  * Return: Nothing.
- *
  */
 
 void print_name_as_is(char *name)
 {
-for (int i = 0;name[i] != '\0'; i++)
-{
- _putchar(name[i]);
-}
+printf("Hello, my name is %s\n", name);
 }
