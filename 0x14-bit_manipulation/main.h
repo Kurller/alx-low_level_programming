68a494e149534eba0795a05b186a8b2de08c146d
Kurller <kolaquadry@gmail.com>
2022-08-19 15:19:54 -0700

    task 7

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <stdbool.h>
+
+
+
+#define  BIT_SIZE 8
+
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -8,6 +20,7 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 int _putchar(char c);
+int _strlen(const char *s);
 
 
 
