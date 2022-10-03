09836285e2ec52b8050f5e85ebe92743f93018dc
Kurller <Kurller>
2022-10-03 16:54:05 -0700

    task 3 done

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Kurller/alx-low_level_progamming/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /tmp https://github.com/Kurller/alx-low_level_progamming/raw/master/0x18-dynamic_libraries/liball.so
+export LD_PRELOAD=/tmp/liball.so
