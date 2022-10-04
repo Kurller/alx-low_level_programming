53394c075f09d1e26e2cc88fe1fd5d5b77ad67f8
Kurller <Kurller>
2022-10-04 00:56:16 -0700

    task 2 done

---
+++ 0x18-dynamic_libraries/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "main.h"
+
+int main()
+{
+printf("The product of a and b is %d, and the divisionof a and b is %d,the subtraction of a and b is %d, modulus of a and b is %d, addition of a and b is %d\n",mul(10, 20), div(30, 2), sub(5, 2), mod(10, 2), add(4, 7));
+return 0;
+}
